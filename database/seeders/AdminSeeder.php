7026db12daa97c8a216ae5532b92c9cdcd074c1b
masudrana03 <920mash@gmail.com>
2024-09-27 23:01:21 +0600

    admin seeder and mailtrap env update

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@admin.com',
+            'email_verified_at' =>now(),
+            'password' => bcrypt('password'),
+            'role' => 'admin',
+        ]);
+
+    }
+}
